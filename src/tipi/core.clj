b5f5ef00d5a6019646c882ad8a802c5b80e4f557
vijaykiran <mail@vijaykiran.com>
2012-10-04 01:48:19 +0200

    Init

---
+++ src/tipi/core.clj
@@ -0,0 +1,6 @@
+(ns tipi.core)
+
+(defn -main
+  "I don't do a whole lot."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/tipi/core_test.clj
@@ -0,0 +1,7 @@
+(ns tipi.core-test
+  (:use clojure.test
+        tipi.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
